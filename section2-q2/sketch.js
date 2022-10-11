b8cd3f45bc60e51a17365f9181f84df9833b949b
Taichi0109 <1930886h@stu.kobe-u.ac.jp>
2022-10-11 17:12:01 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,26 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+     
+     if((i+j)%2==0){
+      noFill();
+     }
+     else{
+      fill(128);
+     }
+     rect(size*i,size*j,25,25);
+     
+     if((i+j)%2==0 || (j>2 && j<5)){
+      noFill();
+     }
+     else if((i+j)%2==0 || j>4){
+      fill(0,0,0)
+     }
+     else{
+      fill(255,0,0);
+     }
+     ellipse(size*i+12.5,size*j+12.5,20);
     }
   }
 }
