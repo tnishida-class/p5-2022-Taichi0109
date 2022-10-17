26bc02359b540655e251cc9ea8f6522c4b6ecf87
Taichi0109 <1930886h@stu.kobe-u.ac.jp>
2022-10-18 03:28:35 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -9,8 +9,35 @@ function setup(){
 }
 
 function draw(){
-  background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  noStroke()
+  if(mouseIsPressed){
+    background(0, 0, 255);
+    fill(255,0,0);
+    if(count < 1/4 * cycle){
+      size = 4 * count;
+    }
+    if(count > 1/4 * cycle && count < 1/2 * cycle){
+      size = 4 * (count - 2 * (count - 1/4 * cycle));
+    }
+    if(count > 1/2 * cycle && count < 3/4 * cycle){
+      size = 4 * (count - 1/2 * cycle)
+    }
+    if(count > 3/4 * cycle){
+      size = 4 * (count - (2 * (count - 3/4 * cycle) + 1/2 * cycle))
+    }
+  }
+  else{
+    background(0, 255, 255);
+    fill(255,0,255);
+    if(count < 1/2 * cycle){
+      size = 2 * count;
+    }
+    if(count > 1/2 * cycle){
+      size = 2 * (count - 2 * (count - 1/2 * cycle));
+    }
+  }
+  
   ellipse(width / 2, height / 2, size);
 }
