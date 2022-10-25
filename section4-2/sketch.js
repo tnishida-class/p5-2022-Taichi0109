ba959fdd454b85459a1341dec325113d55432706
Taichi0109 <1930886h@stu.kobe-u.ac.jp>
2022-10-25 11:12:55 +0900

    Change section 4-2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -11,21 +11,14 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  for(let i = 0; i < balls.length; i++){
-    let b = balls[i];
-    ellipse(b.x, b.y, b.size);
-    b.x += b.vx;
-    b.y += b.vy;
-  }
-}
-
-function mouseDragged(){
-  const dx = mouseX - pmouseX;
-  const dy = mouseY - pmouseY;
-  if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: random(10,100), vx: dx, vy: dy };
+    for(let i = 0; i < balls.length; i++){
+      let b = balls[i];
+      ellipse(b.x, b.y, b.size);
+      b.x += b.vx;
+      b.y += b.vy;
+    }
+    const b = { x: windowWidth / 2, y: windowHeight / 2, size: random(10,100), vx: random(-5,5), vy: random(-5,5)};
     balls.push(b);
-  }
 }
 
 function windowResized(){
